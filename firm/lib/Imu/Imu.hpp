9cdc82dab097f88f081a69b5f786aa4155f50da8
onare <reona.shiode@gmail.com>
2025-01-28 23:02:20 +0900

    refactor: add imu class

---
+++ firm/lib/Imu/Imu.hpp
@@ -0,0 +1,107 @@
+#ifndef _IMU_HPP_
+#define _IMU_HPP_
+
+#include <Kalman.h>
+#include <M5StickCPlus2.h>
+using namespace m5;
+
+class Imu {
+ private:
+  /* data */
+  static const uint32_t taskPeriodMs = 5;
+  static constexpr double dt = 0.005;  // sec
+  Kalman kalmanFilterX, kalmanFilterY;
+  Madgwick filter;
+  xTaskHandle kalmanTaskHandle = 0;
+  xTaskHandle madgwickTaskHandle = 0;
+  imu_3d_t angle = {0};
+  imu_3d_t angleMadgwick = {0};
+  imu_3d_t gyro = {0};
+  imu_3d_t accel = {0};
+
+  static imu_3d_t calcDegreeFromAccel(imu_3d_t accel) {
+    imu_3d_t res = {0};
+    res.x = atan2(accel.y, accel.z) * RAD_TO_DEG;
+    res.y = atan2(-accel.x, sqrt(accel.y * accel.y + accel.z * accel.z)) *
+            RAD_TO_DEG;
+    return res;
+  }
+
+  void createTask() {
+    xTaskCreate(taskEntryKalman, "update imu task kalman",
+                CONFIG_ARDUINO_LOOP_STACK_SIZE, this, tskIDLE_PRIORITY + 1,
+                &kalmanTaskHandle);
+    // xTaskCreate(taskEntryMadgwick, "update imu task madgwick",
+    //             CONFIG_ARDUINO_LOOP_STACK_SIZE, this, tskIDLE_PRIORITY + 1,
+    //             &madgwickTaskHandle);
+  }
+
+  void vUpdateImuMadgwick() {
+    auto sampleFreq = 25.0;
+    auto taskPeriodMs = 40;
+
+    filter.begin(sampleFreq);
+    // Initialize the last wake time
+    auto xLastWakeTime = xTaskGetTickCount();
+    while (1) {
+      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(taskPeriodMs));
+      filter.updateIMU(gyro.x, gyro.y, gyro.z, accel.x, accel.y, accel.z);
+    }
+  }
+
+  void vUpdateImuKalman() {
+    auto xLastWakeTime = xTaskGetTickCount();
+    auto initialized = false;
+    while (1) {
+      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(taskPeriodMs));
+      M5.Imu.update();
+      auto data = M5.Imu.getImuData();
+      auto angle = calcDegreeFromAccel(data.accel);
+      if (!initialized) {
+        kalmanFilterX.setAngle(angle.x);
+        kalmanFilterY.setAngle(angle.y);
+        initialized = !initialized;
+      } else {
+        this->angle.x = kalmanFilterX.getAngle(angle.x, data.gyro.x, dt);
+        this->angle.y = kalmanFilterY.getAngle(angle.y, data.gyro.y, dt);
+        this->gyro = data.gyro;
+        this->accel = data.accel;
+      }
+    }
+  }
+
+  static void taskEntryKalman(void* arg) {
+    static_cast<Imu*>(arg)->vUpdateImuKalman();
+  }
+  static void taskEntryMadgwick(void* arg) {
+    static_cast<Imu*>(arg)->vUpdateImuMadgwick();
+  }
+
+ public:
+  Imu() {
+    M5.Imu.begin();
+    createTask();
+    delay(taskPeriodMs * 2);
+  };
+  ~Imu() {
+    if (kalmanTaskHandle) {
+      vTaskDelete(kalmanTaskHandle);
+    }
+    if (madgwickTaskHandle) {
+      vTaskDelete(madgwickTaskHandle);
+    }
+  }
+
+  imu_3d_t getAngle() { return angle; }
+  imu_3d_t getAngleMadgwick() {
+    imu_3d_t res = {0};
+    res.x = filter.getRoll();
+    res.y = filter.getPitch();
+    res.z = filter.getYaw();
+    return res;
+  }
+  imu_3d_t getGyro() { return gyro; }
+  imu_3d_t getAccel() { return accel; }
+};
+
+#endif
--- firm/src/main.cpp
+++ firm/src/main.cpp
@@ -3,68 +3,12 @@
 #include <M5StickCPlus2.h>
 #include <MadgwickAHRS.h>
 
+#include <Imu.hpp>
 #include <Motor.hpp>
 #include <PwmController.hpp>
 
 std::unique_ptr<Motor> motor;
-Madgwick filter;
-m5::IMU_Class::imu_3d_t kalmanAngle;
-
-void vUpdateImu(void *pvParameters) {
-  TickType_t xLastWakeTime;
-  M5.Imu.begin();
-  xLastWakeTime = xTaskGetTickCount();
-  while (1) {
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1));
-    M5.Imu.update();
-  }
-}
-
-void vUpdateImuMadgwick(void *pvParameters) {
-  TickType_t xLastWakeTime;
-
-  // Initialize the last wake time
-  filter.begin(25);  // TODO
-  xLastWakeTime = xTaskGetTickCount();
-  while (1) {
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(40));
-    auto data = M5.Imu.getImuData();
-    filter.updateIMU(data.gyro.x, data.gyro.y, data.gyro.z, data.accel.x,
-                     data.accel.y, data.accel.z);
-  }
-}
-
-m5::imu_3d_t calcDegreeFromAccel(m5::imu_3d_t accel) {
-  m5::imu_3d_t res;
-  res.x = atan2(accel.y, accel.z) * 180 / PI;
-  res.y =
-      atan2(-accel.x, sqrt(accel.y * accel.y + accel.z * accel.z)) * 180 / PI;
-  return res;
-}
-
-void vUpdateImuKalman(void *pvParameters) {
-  TickType_t xLastWakeTime;
-  std::unique_ptr<Kalman> kalmanFilterX, kalmanFilterY;
-
-  auto dt = 0.005;  // 5msec
-
-  // Initialize the last wake time
-  xLastWakeTime = xTaskGetTickCount();
-  while (1) {
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(5));
-    auto data = M5.Imu.getImuData();
-    auto angle = calcDegreeFromAccel(data.accel);
-    if (!kalmanFilterX) {
-      kalmanFilterX.reset(new Kalman());
-      kalmanFilterX->setAngle(angle.x);
-      kalmanFilterY.reset(new Kalman());
-      kalmanFilterY->setAngle(angle.y);
-    } else {
-      kalmanAngle.x = kalmanFilterX->getAngle(angle.x, data.gyro.x, dt);
-      kalmanAngle.y = kalmanFilterY->getAngle(angle.y, data.gyro.y, dt);
-    }
-  }
-}
+std::unique_ptr<Imu> imu;
 
 void vUpdateDisplay(void *pvParameters) {
   char str[200];
@@ -73,7 +17,10 @@ void vUpdateDisplay(void *pvParameters) {
   M5.Display.setFont(&fonts::Orbitron_Light_24);
   M5.Display.setTextSize(0.5);
   for (;;) {
-    auto data = M5.Imu.getImuData();
+    auto gyro = imu->getGyro();
+    auto angle = imu->getAngle();
+    auto angleMadgwick = imu->getAngleMadgwick();
+    auto accel = imu->getAccel();
     sprintf(str,
             "%6.2f  %6.2f  %6.2f\t\n"
             "\t  \t o/s \n\n"
@@ -81,9 +28,8 @@ void vUpdateDisplay(void *pvParameters) {
             "\t  \t o/ madgewick\n\n"
             "%6.2f  %6.2f  %6.2f\t \n"
             "\t  \t o/ kalman",
-            data.gyro.x, data.gyro.y, data.gyro.z, filter.getRoll(),
-            filter.getPitch(), filter.getYaw(), kalmanAngle.x, kalmanAngle.y,
-            kalmanAngle.z);
+            gyro.x, gyro.y, gyro.z, angleMadgwick.x, angleMadgwick.y,
+            angleMadgwick.z, angle.x, angle.y, 0);
     M5.Display.setCursor(0, 10);
     M5.Display.print(str);
     delay(500);
@@ -94,22 +40,12 @@ void setup() {
   auto config = M5.config();
   config.serial_baudrate = 115200;
   M5.begin(M5.config());
-  motor.reset(Motor::create(IoPins::IO0, IoPins::IO26));
-  xTaskCreateUniversal(vUpdateImu, "update imu task",
-                       CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL,
-                       tskIDLE_PRIORITY + 2, NULL, 1);
-  xTaskCreateUniversal(vUpdateImuMadgwick, "update imu task by madgwick",
-                       CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL,
-                       tskIDLE_PRIORITY + 1, NULL, 1);
-  xTaskCreateUniversal(vUpdateImuKalman, "update imu task by kalman",
-                       CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL,
-                       tskIDLE_PRIORITY + 1, NULL, 1);
+  // motor.reset(Motor::create(IoPins::IO0, IoPins::IO26));
+  imu.reset(new Imu());
   xTaskCreateUniversal(vUpdateDisplay, "update display task",
                        CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL,
                        tskIDLE_PRIORITY + 4, NULL, 1);
 }
-
-m5::IMU_Class::imu_3d_t offset;
 
 void loop() {}
 
