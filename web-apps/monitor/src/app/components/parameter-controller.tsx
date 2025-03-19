cfa7a3761cbcdf9a9530b75b3741defff8f936d8
onare <reona.shiode@gmail.com>
2025-03-19 22:10:51 +0900

    feat(monitor): use dialog to change parameter

--- web-apps/monitor/src/app/components/parameter-controller.tsx
+++ web-apps/monitor/src/app/components/parameter-controller.tsx
@@ -6,6 +6,17 @@ import { Label } from "@/components/ui/label";
 import { use, useCallback, useState } from "react";
 import { ControlData, useControlData } from "../hooks/useControlData";
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { toast } from "sonner";
+
 const defaultControlData: ControlData = {
   ki: 0.1,
   kd: 0.1,
@@ -23,7 +34,7 @@ const InputControlParam = ({
   onChange?: (value: number) => void;
 }) => {
   return (
-    <div className="flex items-center space-x-2">
+    <div className="space-y-2">
       <Label htmlFor={label}>{label}</Label>
       <Input
         id={label}
@@ -47,41 +58,62 @@ const ParameterController = () => {
   );
 
   const handleSubmit = useCallback(async () => {
-    await updateControlData(data);
+    const promise = updateControlData(data);
+    toast.promise(promise, {
+      loading: "Loading...",
+      success: () => {
+        return "Parameters updated";
+      },
+      error: "Failed to update parameters",
+    });
   }, [data]);
 
   return (
-    <div className="space-x-5 flex">
-      <InputControlParam
-        label="ki"
-        value={data.ki}
-        onChange={(ki) => {
-          setData({ ...data, ki: ki });
-        }}
-      />
-      <InputControlParam
-        label="kd"
-        value={data.kd}
-        onChange={(kd) => {
-          setData({ ...data, kd: kd });
-        }}
-      />
-      <InputControlParam
-        label="kp"
-        value={data.kp}
-        onChange={(kp) => {
-          setData({ ...data, kp: kp });
-        }}
-      />
-      <InputControlParam
-        label="target"
-        value={data.target}
-        onChange={(target) => {
-          setData({ ...data, target: target });
-        }}
-      />
-      <Button onClick={handleSubmit}>Submit</Button>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default">Change Parameters</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Change Parameters</DialogTitle>
+        </DialogHeader>
+        <div className="space-y-4">
+          <InputControlParam
+            label="ki"
+            value={data.ki}
+            onChange={(ki) => {
+              setData({ ...data, ki: ki });
+            }}
+          />
+          <InputControlParam
+            label="kd"
+            value={data.kd}
+            onChange={(kd) => {
+              setData({ ...data, kd: kd });
+            }}
+          />
+          <InputControlParam
+            label="kp"
+            value={data.kp}
+            onChange={(kp) => {
+              setData({ ...data, kp: kp });
+            }}
+          />
+          <InputControlParam
+            label="target"
+            value={data.target}
+            onChange={(target) => {
+              setData({ ...data, target: target });
+            }}
+          />
+        </div>
+        <DialogFooter className="flex justify-end mt-10">
+          <DialogClose asChild>
+            <Button onClick={handleSubmit}>Submit</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- web-apps/monitor/tailwind.config.ts
+++ web-apps/monitor/tailwind.config.ts
@@ -8,10 +8,10 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
       fontFamily: {
         Noto_Sans: ["Noto Sans", "sans-serif"],
       },
